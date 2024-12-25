b1a418ca2202c9b63e85d772a943d05d814b21b6
Александр Михалев <60660331+Flipmachin1001@users.noreply.github.com>
2024-12-25 12:18:41 +0300

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Lbl_DateTime.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void btn_Refresh_Clicked(object sender, EventArgs e)
+        {
+            Lbl_DateTime.Text = DateTime.Now.ToShortDateString();
+        }
+    }
+}
